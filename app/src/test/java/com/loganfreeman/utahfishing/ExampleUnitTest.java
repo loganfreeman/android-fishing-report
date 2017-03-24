9840a38db8d09ee44ef93c3beb94e2019c2c08a1
shanhong cheng <shnahongcheng@gmail.com>
2017-03-23 23:33:39 -0600

    all

--- app/src/test/java/com/loganfreeman/utahfishing/ExampleUnitTest.java
+++ app/src/test/java/com/loganfreeman/utahfishing/ExampleUnitTest.java
@@ -1,8 +1,16 @@
 package com.loganfreeman.utahfishing;
 
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+
 import org.junit.Test;
 
+import java.text.ParseException;
+import java.util.Date;
+
 import static org.junit.Assert.*;
+
+import static com.loganfreeman.utahfishing.common.utils.Time.df;
+
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
@@ -14,7 +22,11 @@ public class ExampleUnitTest {
 
 
     @Test
-    public  void testCalendarDay() {
-
+    public  void testCalendarDay() throws ParseException {
+        Date d = df.parse("03/11/2017");
+        CalendarDay calendarDay = CalendarDay.from(d);
+        assertEquals(calendarDay.getDay(), 11);
+        assertEquals(calendarDay.getMonth(), 3);
+        assertEquals(calendarDay.getYear(), 2017);
     }
 }
